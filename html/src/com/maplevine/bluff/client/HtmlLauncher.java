f7a9371958ff2a2235e8f01001cb39f0e51ef9c7
Jeremy (Jeung Hwan) Suh <jeunghwansuh@gmail.com>
2017-07-23 04:13:15 +0700

    Added general files

---
+++ html/src/com/maplevine/bluff/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.maplevine.bluff.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.maplevine.bluff.Bluff;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Bluff();
+        }
+}
